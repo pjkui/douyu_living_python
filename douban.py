473f78eaa477c7f873823cfc519f1cf2b9e0a90b
quinn pan <pjkui@qq.com>
2017-01-02 18:19:23 +0800

    添加查询豆瓣高分电影

--- douban.py
+++ douban.py
@@ -3,10 +3,15 @@ import requests
 
 
 
-def GetMovies(start,count):
+def GetMovies(start,count,movie_type=u'热门'):
+    
     #豆瓣上电影的评分的ＵＲＬ之一
+    """
     doubanUrl = 'https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=rank&page_limit='+str(count)+'&page_start='+str(start)
     
+    doubanUrl = 'https://movie.douban.com/j/search_subjects?type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=rank&page_limit=20&page_start=0'
+    """
+    doubanUrl = 'https://movie.douban.com/j/search_subjects'
     print('url:',doubanUrl)
     """
     参数的解释：
@@ -16,12 +21,31 @@ def GetMovies(start,count):
     '&page_limit=20'+#每一页的限制查询数量，我想我们可以改，来改改试试。。。
     '&page_start=0'
     """
-    r = requests.get(doubanUrl)
+    
+    payload = {'type': 'movie',
+               'tag' : movie_type,
+               'sort':'rank',
+               'page_limit': count,
+               'page_start':start
+               }
+
+#        r = requests.get('http://httpbin.org/get', params=payload)
+
+    r = requests.get(doubanUrl,params=payload)
+    print(r.url)
     data = r.json()
-    print('data length:',len(data))
+    
+    print(u'查到了你想要的数据。。')
+    fileMode = None
+    if(start is 0):
+        fileMode = "w"
+    else:
+        fileMode = "a+"
     if len(data) == 1:
         subjects = data['subjects']
-        print('subjects length(查询到的电影数量：):', len(subjects))
+        
+        fileHandler = open('豆瓣'+movie_type+'电影.txt',fileMode)
+        print('一共查询到:', len(subjects),'部电影。')
         #开始解析查询到的电影信息
         """
         {'cover': 'https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2315672647.jpg',
@@ -38,19 +62,28 @@ def GetMovies(start,count):
          豆瓣上具体的详细介绍的地址
          """
         for movie in subjects:
-            print(movie['title'],'评分:',movie['rate'])
+            str_line = '['+movie['title']+'] 评分:'+movie['rate']+' 详情地址：'+movie['url']+'\n'
+            print(str_line)
+            fileHandler.write(str_line)
             #这个时候基本上可以获得一些我们想要的信息了，但是，我并不满足。多获得一些电影试试
-            
+        fileHandler.close()
             
 #我们将上面的代码封装成一个函数，用来获取我们想要的电影
 #函数名GetMovies(参数1指定从多少位开始获得，参数2指定一次获得电影的数量)
 #额。。。备份一下代码，如果想玩的话，一会儿自己可以下载一下自己本地跑一下
 def main():
-    GetMovies(1,40) #现在只是测试调用，test：OK!
+    #萌萌的查询200个数据试一下
+    GetMovies(0,100,u'热门') #现在只是测试调用，test：OK!
+    GetMovies(0,100,u'豆瓣高分') 
     #现在开始动态关联
-    GetMovies(40,40)
+    #GetMovies(1000,1000)
+    
     #单纯了，好像没有查到40个数据
     #bug fixed！因为参数中加入了空格
     #备份一下代码
+    #突然发现，做到这样基本就可以了。如果想看详细信息可以去豆瓣上看。
+    #如果我再去获得每一个电影的概述，感觉意义不太大
+    #来看看是不是可以获取前1000部电影
+    
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-##去豆瓣获得高分电影的URL
+##去豆瓣获得热门高分电影的URL
 https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=rank&page_limit=20&page_start=0
 
 已经可以初步下载一些电影的信息了
@@ -9,3 +9,17 @@ https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sor
 第二步：添加函数
 函数添加完毕了，大家可以继续下载。
 第三步，将评分数据写入到一个本地文件，方便自己随时看。
+好了，废话不多说。直接第三步。
+代码所在地址：https://github.com/pjkui/douyu_living_python
+
+第四步：获取豆瓣高分电影。刚刚是豆瓣的热门电影
+地址：https://movie.douban.com/j/search_subjects?type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=rank&page_limit=20&page_start=0
+来，做这一步
+ 搞定了
+第五步：需求来了、、、、、
+    用更优雅的方式去构建请求URL.
+    还是做这一步吧，这一步做好后做第四步会更简单点儿
+    搞定了
+ＴＯＤＯ：
+第六步：
+    上传一下代码
